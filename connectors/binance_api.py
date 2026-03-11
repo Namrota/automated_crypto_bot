d9524cc4b1bdd13e655b02004a2df284342a1c92
Namrota Ghosh <namrotaghosh.dds15@iiitb.net>
2026-03-11 15:09:07 +0100

    tested and added DOTUSDT crypto

--- connectors/binance_api.py
+++ connectors/binance_api.py
@@ -78,4 +78,4 @@ def get_24hr_ticker(symbol:str)-> dict:
     response_obj.raise_for_status()
     return response_obj.json()
 
-print(get_24hr_ticker(symbol="PAXGUSDT"))
+print(get_24hr_ticker(symbol="DOTUSDT"))
