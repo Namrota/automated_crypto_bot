9e886d50e98139be8352bd7158dfc3d4321e68e7
Namrota Ghosh <namrotaghosh.dds15@iiitb.net>
2026-03-11 12:26:29 +0100

    cleaner coding approach: storing constants and urls in settings.py

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+
+# Trading Tickers Default (comma-separated)
+SPOT_TICKERS=['BTCUSDT','ETHUSDT','SOLUSDT','XRPUSDT','BNBUSDT']
+
+
+# Base Endpoints:
+# All endpoints return either a JSON object or array
+# Data is returned in Ascennding order (oldest to newest)
+BASE_URL= "https://fapi.binance.com"
+TESTNET_REST_URL= "https://demo-fapi.binance.com"
+TESTNET_WEBSOCKET_URL= "wss://fstream.binancefuture.com"
+
+#URLs:
+EXCHANGE_INFO= "/fapi/v1/exchangeInfo"
